58fb9e2663b4ecea3addfbd3958f86b526def7b3
Ciaozzu <89047433+Ciaozzu@users.noreply.github.com>
2021-10-17 12:38:36 -0700

    Update Reto.py

--- Reto.py
+++ Reto.py
@@ -1,3 +1,41 @@
-def ventas()
-  print("Mostrar reporte de ventas por vendedor o por articulo
-  if resp
+import os
+def regV(nombreArch): #Registra nuestras ventas
+    textoVentas = open(nombreArch + "txt", "a")
+    textoVentas.write(creadorRegV())
+    textoVentas.close()
+    
+def creadorRegV(): #Hace el string necesario regV
+    
+    return registro
+    regV()
+    
+def regVE(): #Agrega la venta realizada del empleado al registro
+
+def regLA(): #Registra la llegada de artículos
+    
+    
+def agreI(): #Agrega el artículo registrado al inventario
+    
+    open( "a"
+    return
+
+def consultI(): #Consulta los datos del  inventario
+    return
+          
+def consultV #Consulta los datos de las ventas
+    return
+
+
+def reporteVA():
+  print("Mostrar reporte de ventas por:  1. Por vendedor 2. Por articulo")
+  resp = input("")
+  if resp == 1
+      juan Karla Sam Margaret Jose Henry
+  elif resp == 2
+      Articulo
+          
+def prodPA(): #Te despliega los productos que estan por agotarse
+    
+
+def menu():
+    creadorRegV
