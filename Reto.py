c51e5e2ebdf3b1593f6ee31e244663a50614f0ec
Ciaozzu <89047433+Ciaozzu@users.noreply.github.com>
2021-10-17 14:42:25 -0700

    Update Reto.py

--- Reto.py
+++ Reto.py
@@ -1,41 +1,38 @@
 import os
-def regV(nombreArch): #Registra nuestras ventas
-    textoVentas = open(nombreArch + "txt", "a")
-    textoVentas.write(creadorRegV())
-    textoVentas.close()
+print(os.getcwd())
+
+def regV(nombreArch, crv): #Registra nuestras ventas
+    with open(nombreArch + ".txt", "a") as textoVentas:
+        textoVentas.write(crv)
     
 def creadorRegV(): #Hace el string necesario regV
-    
+    registro = "hola"
     return registro
-    regV()
-    
-def regVE(): #Agrega la venta realizada del empleado al registro
+    #regV()
+regV("ventas", creadorRegV())    
+#def regVE(): #Agrega la venta realizada del empleado al registro
 
-def regLA(): #Registra la llegada de artículos
+#def regLA(): #Registra la llegada de artículos
     
     
-def agreI(): #Agrega el artículo registrado al inventario
-    
-    open( "a"
-    return
+#def agreI(): #Agrega el artículo registrado al inventario
 
-def consultI(): #Consulta los datos del  inventario
-    return
+#def consultI(): #Consulta los datos del  inventario
           
-def consultV #Consulta los datos de las ventas
-    return
+#def consultV #Consulta los datos de las ventas
 
 
-def reporteVA():
-  print("Mostrar reporte de ventas por:  1. Por vendedor 2. Por articulo")
-  resp = input("")
-  if resp == 1
-      juan Karla Sam Margaret Jose Henry
-  elif resp == 2
-      Articulo
+#def reporteVA():
+  #print("Mostrar reporte de ventas por:  1. Por vendedor 2. Por articulo")
+  #resp = input("")
+  #if resp == 1
+      #juan Karla Sam Margaret Jose Henry
+  #elif resp == 2
+      #Articulo
           
-def prodPA(): #Te despliega los productos que estan por agotarse
+#def prodPA(): #Te despliega los productos que estan por agotarse
     
 
-def menu():
-    creadorRegV
+#def menu():
+    
+    #regV("ventas", creadorRegV())
